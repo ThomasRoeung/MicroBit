3bb6761d471e4d6ed9bd39ca771a422af8e7e0bc
ThomasRoeung <109984544+ThomasRoeung@users.noreply.github.com>
2023-03-07 14:15:46 -0500

    s

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+input.onButtonPressed(Button.A, function () {
+    rope += -0.1
+})
+input.onButtonPressed(Button.B, function () {
+    rope += 0.1
+})
+let rope = 2
+let ropey = 2
+basic.forever(function () {
+    basic.clearScreen()
+    led.plot(rope, ropey)
+})
