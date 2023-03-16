c8e8de33e3122e5c594a670ab379fb7d1fd61dfc
ThomasRoeung <109984544+ThomasRoeung@users.noreply.github.com>
2023-03-15 21:29:27 -0400

    sada

--- main.ts
+++ main.ts
@@ -4,9 +4,22 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     rope += 0.1
 })
+let winsa = 0
 let rope = 2
 let ropey = 2
+let winsb = 0
+winsa += 0
 basic.forever(function () {
     basic.clearScreen()
     led.plot(rope, ropey)
+    if (rope < 0) {
+        winsa += 1
+        basic.showNumber(winsa)
+        rope = 2
+    }
+    if (rope > 5) {
+        winsb += 1
+        basic.showNumber(winsb)
+        rope = 2
+    }
 })
